99ceee37abc20951f3c4d15ac1aaee111d19a4fd
stonksadmin <stonksswapfinance@gmail.com>
2021-03-08 11:12:18 +0200

    text color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -23,7 +23,7 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#452A7A",
+  text: "#000000",
   textDisabled: "#BDC2C4",
   textSubtle: "#8f80ba",
   borderColor: "#E9EAEB",
